ebf1570b59cc129f39607cdfbdf653694499dc27
CodeXaver <mitchel.okomor@gmail.com>
2020-06-10 02:52:15 +0100

    Created user model

---
+++ controller/users.js
@@ -0,0 +1,131 @@
+const User = require("../models/user");
+
+//create a user
+const user = {
+  addUser: async (req, res) => {
+    const fName = req.body.fName.trim();
+    const lName = req.body.lName.trim();
+    const number = req.body.number.trim();
+    const address = req.body.address.trim();
+    const email = req.body.email.trim();
+
+    //create a new instance of user
+    const newUser = new User(fName, lName, number, address, email);
+
+    try {
+      newUser.addOne((result) => {
+        if (result.insertId) {
+          res.status(200).json({
+            status: "success",
+            message: "Record added successfuly",
+            data: result.insertId,
+          });
+        } else {
+          res.status(501).json({
+            status: "error",
+            message: "failed to add user",
+          });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  getUserr: (req, res) => {
+    console.log(req.params.id);
+    const user = new User();
+
+    try {
+      user.getOne(req.params.id, (result) => {
+        if (result.length > 0) {
+          res.status(200).json({
+            status: "success",
+            data: result,
+          });
+        } else {
+          res.status(501).json({
+            status: "error",
+            message: "No user found",
+          });
+        }
+      });
+    } catch (err) {
+        console.log(err);
+    }
+  },
+
+  getAllUsers: (req, res) => {
+    const user = new User();
+
+    try {
+      user.getAll( (result) => {
+        if (result.length > 0) {
+          res.status(200).json({
+            status: "success",
+            data: result,
+          });
+        } else {
+          res.status(501).json({
+            status: "error",
+            message: "No user found",
+          });
+        }
+      });
+    } catch (err) {
+        console.log(err);
+    }
+  },
+
+
+deleteUser: (req, res)=>{
+    const user = new User();
+    try {
+      user.deleteOne(req.params.id, (result) => {
+        if (result.affectedRows > 0) {
+          res.status(200).json({
+            status: "success",
+            message: "user deleted successfuly",
+          });
+        } else {
+          res.status(501).json({
+            status: "error",
+            message: "No user found",
+          });
+        }
+      });
+    } catch (err) {
+        console.log(err);
+    }
+},
+
+updateUser: (req, res)=>{
+    const name = req.body.name.trim();
+    const number = req.body.number.trim();
+    const address = req.body.address.trim();
+    const email = req.body.email.trim();
+
+    //create a new instance of user
+    const user = new User(name, number, address, email);    
+    try {
+      user.updateOne(req.params.id, (result) => {
+        if (result.affectedRows > 0) {
+          res.status(200).json({
+            status: "success",
+            message: "user updated successfuly",
+          });
+        } else {
+          res.status(501).json({
+            status: "error",
+            message: "Failed to update user",
+          });
+        }
+      });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+};
+
+module.exports = user;
---
+++ models/user.js
@@ -0,0 +1,87 @@
+const db = require('../services/db');
+
+class User {
+    constructor(fName, lName, number, address, email){
+        this.fName = fName;
+        this.lName  = lName;
+        this.number = number;
+        this.address = address;
+        this.email = email;
+    }
+
+set user (user){
+    this.fName =user.fName;
+    this.lName = user.lName;
+    this.number =user.number;
+    this.address =user.address;
+    this.email = user.email;
+}
+
+get user (){
+    return ([this.fName, this.lName, this.number, this.address, this.emial]);
+}
+
+ addOne(callback){
+     
+    console.log("request recieved");
+const queryString = `INSERT INTO user (firstname, lastname number, address, email) values ('${this.fName}', '${this.lName}', '${this.number}', '${this.address}','${this.email}' )`;
+db.query(queryString, (err, result)=>{
+    if(err){
+        console.log(err);
+    }
+callback(result);
+});
+
+}
+
+getOne(id, callback){
+const queryString = `SELECT * FROM user WHERE id = ${id}`;
+db.query(queryString, (err, result)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+    callback(result);
+    }
+});
+}
+
+
+getAll(callback){
+    const queryString = `SELECT * FROM user`;
+db.query(queryString, (err, result)=>{
+    if(err){
+       throw err;
+    }
+    else{
+    callback(result);
+    }
+});
+
+}
+
+updateOne(id, callback){
+const queryString = `UPDATE user SET firstname ='${this.fName}', lastname ='${this.lName}', number='${this.number}', address='${this.address}', email='${this.email}' WHERE id = ${id}`;
+db.query(queryString, (err, result)=>{
+    if(err){
+        throw err;
+    }
+callback(result);
+});    
+}
+
+deleteOne(id, callback){
+    const queryString = `DELETE FROM user WHERE id = ${id}`;
+    db.query(queryString, (err, result)=>{
+        if(err){
+           throw err;
+        }
+        else{
+        callback(result);
+        }
+    });
+}
+
+}
+
+module.exports = User;
